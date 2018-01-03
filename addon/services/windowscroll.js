d9a1ad8a99c74343e2dcbe30a4393ece721dcd79
Michael North <michael.l.north@gmail.com>
2018-01-02 22:51:38 -0800

    Prettier

--- addon/services/windowscroll.js
+++ addon/services/windowscroll.js
@@ -6,7 +6,7 @@ export default Service.extend(Evented, {
   scrollTop: null,
   init() {
     this._super(...arguments);
-    $(window).on('scroll', (e) => {
+    $(window).on('scroll', e => {
       this._onScroll(e, $(window).scrollTop());
     });
     this.set('scrollTop', $(window).scrollTop());
